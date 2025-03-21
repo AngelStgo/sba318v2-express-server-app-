5c97c46c1d99494b95a040a0283f3ec8ce9318d1
AngelStgo <euangel.stgo@gmail.com>
2025-03-21 01:53:36 -0400

    organized code

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ import orderRouter from './routes/orders.js';
 const app = express();
 const PORT = 5050;
 
-// Convert file paths correctly for ES modules
+// Convert file paths to ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
+// Middleware 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger);
 app.use(customHeader);
+// Error Handling Middleware
+app.use(errorHandler);
 
 // Set View Engine
 app.set('view engine', 'ejs');
@@ -39,15 +41,12 @@ app.use('/users', userRouter);
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
 
-// Home Route (Renders View)
+// Home Route 
 app.get('/', (req, res) => {
     res.render('index', { title: 'API Overview' });
 });
 
-// Error Handling Middleware
-app.use(errorHandler);
 
-// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
